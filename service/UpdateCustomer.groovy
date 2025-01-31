20b67970f66d7ec6d54795198caa2bdb5ddf7fa1
Banibrata Manna <mbanibrata668@gmail.com>
2025-01-31 21:07:58 +0530

    Bug Fix
    Fixed bug in Update Customer groovy file

--- service/UpdateCustomer.groovy
+++ service/UpdateCustomer.groovy
@@ -1,77 +1,90 @@
 import org.moqui.context.ExecutionContext;
-import org.moqui.entity.EntityValue;
 import org.moqui.entity.EntityCondition;
 import org.moqui.entity.EntityFind;
 import org.moqui.entity.EntityList;
+import org.moqui.entity.EntityValue;
 
 import java.sql.Date
 import java.time.LocalDate;
 
 ExecutionContext ec = context.ec;
 
-Map<String, Object> customer = ec.service.sync().name("PartyServices.find#FindCustomerView").parameter("emailAddress", infoString).call();
+Map<String, Object> customer = ec.service.sync().name("PartyServices.find#FindCustomerView").parameters(["emailAddress" : context.emailAddress]).call();
 
-//if(!customer.partyIdList) {System.exit(0);}
+if(!customer.partyIdList) return;
 
 def partyId = customer.partyIdList.get(0);
 
-//if(!partyId) System.exit(0);
-
-EntityFind ef = ec.entity.find("partyEntities.party.contact.PartyContactMech");
+EntityFind ef = ec.entity.find("partyEntities.party.contact.PartyContactMech").distinct(true);
 
 EntityCondition pId = ec.entity.conditionFactory.makeCondition("partyId",
-        EntityCondition.LIKE, partyId);
+        EntityCondition.EQUALS, partyId);
 
 EntityCondition activeCMech = ec.entity.conditionFactory.makeCondition("thruDate",
-        EntityCondition.EQUALS, null);
+        EntityCondition.IS_NULL, null);
 
 EntityCondition posPri = ec.entity.conditionFactory.makeCondition("contactMechPurposeId",
-        EntityCondition.LIKE, "PostalPrimary");
+        EntityCondition.EQUALS, "PostalPrimary");
 
-ef.condition(pId);
-ef.condition(activeCMech);
-ef.condition(posPri);
+EntityCondition telePri = ec.entity.conditionFactory.makeCondition("contactMechPurposeId",
+        EntityCondition.EQUALS, "PhonePrimary");
+
+EntityCondition primaryCMechs = ec.entity.conditionFactory.makeCondition(
+        posPri, EntityCondition.OR, telePri
+);
+
+List<EntityCondition> conditionList = new ArrayList<>();
+conditionList.add(pId);
+conditionList.add(activeCMech);
+conditionList.add(primaryCMechs);
+
+ef.condition(ec.entity.conditionFactory.makeCondition(
+        conditionList
+));
 
 EntityList entityList = ef.list();
 
-if(address1 && city && postaLCode){
+if(address1){
+    if (!city) return;
+    if (!postalCode) return;
     for(EntityValue ev in entityList) {
         if (ev.contactMechPurposeId == "PostalPrimary")
         ev.set("thruDate", Date.valueOf(LocalDate.now())).update();
     }
 
-    EntityValue ev = ec.entity.makeValue("ContactMech").set("contactMechTypeEnumId", "POSTAL_ADDRESS").setSequencedIdPrimary().create();
-    EntityValue newPostal = ec.entity.makeValue("PostalAddress");
-    newPostal.set("contactMechId", ev.get("contactMechId"));
+        EntityValue ev = ec.entity.makeValue("ContactMech").set("contactMechTypeEnumId", "POSTAL_ADDRESS").setSequencedIdPrimary().create();
+        EntityValue newPostal = ec.entity.makeValue("PostalAddress");
+        createdCMechId = ev.get("contactMechId");
+        newPostal.set("contactMechId", createdCMechId);
+        newPostal.setAll(context).create();
+        ev = ec.entity.makeValue("PartyContactMech");
+        ev.set("partyId", partyId);
+        ev.set("contactMechId", createdCMechId);
+        ev.set("contactMechPurposeId", "PostalPrimary");
+        ev.set("fromDate", Date.valueOf(LocalDate.now())).create();
+    }
+
+if(countryCode){
+
+    if(!areaCode) return;
+    if(!contactNumber) return ;
+
+    for(EntityValue ev in entityList) {
+        if (ev.contactMechPurposeId == "PhonePrimary")
+        ev.set("thruDate", Date.valueOf(LocalDate.now())).update();
+    }
+
+    EntityValue ev = ec.entity.makeValue("ContactMech").set("contactMechTypeEnumId", "TELECOM_NUMBER").setSequencedIdPrimary().create();
+    EntityValue newPostal = ec.entity.makeValue("TelecomNumber");
+    createdCMechId = ev.get("contactMechId");
+    newPostal.set("contactMechId", createdCMechId);
     newPostal.setAll(context).create();
     ev = ec.entity.makeValue("PartyContactMech");
     ev.set("partyId", partyId);
-    ev.set("contactMechId", ev.get("contactMechId"));
-    ev.set("contactMechPurposeId", "PostalPrimary");
+    ev.set("contactMechId", createdCMechId);
+    ev.set("contactMechPurposeId", "PhonePrimary");
     ev.set("fromDate", Date.valueOf(LocalDate.now())).create();
 }
-
-//if(countryCode && areaCode && contactNumber){
-//    EntityCondition telePri = ec.entity.conditionFactory.makeCondition("contactMechPurposeId",
-//            EntityCondition.LIKE, "TelecomPrimary");
-//
-//    ef2.condition(telePri);
-//
-//    EntityList entityList = ef.list();
-//
-//    for(EntityValue ev in entityList) {
-//        ev.set("thruDate", Date.valueOf(LocalDate.now())).update();
-//    }
-//
-//    EntityValue ev = ec.makeValue("ContactMech").set("contactMechTypeEnumId", "TELECOM_NUMBER").setSequencedIdPrimary().create();
-//    EntityValue newTelecom = ec.entity.makeValue("TelecomNumber");
-//    newTelecom.setAll(context).create();
-//    ev = ec.entity.makeValue("PartyContactMech");
-//    ev.set("partyId", partyId);
-//    ev.set("contactMechId", ev.get("contactMechId"));
-//    ev.set("contactMechPurposeId", "PhonePrimary");
-//    ev.set("fromDate", Date.valueOf(LocalDate.now())).create();
-//}
 
 
 
